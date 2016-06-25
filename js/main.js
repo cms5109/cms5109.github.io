a1d3cfb8084f94870d7e1e096c3f33f3acaf8cd7
Conal <cms5109@gmail.com>
2016-06-25 12:46:38 -0400

    Initial site. Bootstrap, sass variables, a few pages and one timeline

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+ $('.collapse')
+     .on('shown.bs.collapse', function() {
+         $(this)
+             .parent()
+             .find(".fa-caret-down")
+             .removeClass("fa-caret-down")
+             .addClass("fa-caret-up");
+         })
+     .on('hidden.bs.collapse', function() {
+         $(this)
+             .parent()
+             .find(".fa-caret-up")
+             .removeClass("fa-caret-up")
+             .addClass("fa-caret-down");
+         });
+     });
